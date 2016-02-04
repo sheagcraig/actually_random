2c8ded144df1f3a6a56a0a21e7bcdd173e81dfb6
Shea Craig <sheagcraig@gmail.com>
2016-02-03 19:31:18 -0500

    Make big changes.
    - Better docstring.
    - Clean out copied stuff that is no longer useful.
    - pylint
    - Start building playlist selection and playlist display views.

--- actually_random.py
+++ actually_random.py
@@ -14,16 +14,25 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Spotify's "shuffle" play feature is not really random. The algorithm
-is not public information, but some tracks get played more frequently
-than others. This script allows you to copy an existing playlist after
-randomizing the tracks. You can then play without shuffle and get the
-equivalent of what you actually want.
+"""I like to rip mountain bike trails to my giant Spotify playlist,
+"Bottomless Happiness". Unfortunately, Spotify's "shuffle" play feature
+is not really random. The algorithm is not public information, but some
+tracks get played more frequently than others. If you play the playlist
+during a ride one day, then ride the next day, chances are extremely
+high that you'll hear a lot of the same songs again.
+
+This is annoying to me.
+
+I want all of my jams, and I want them to not repeat until I have had
+the auditory satisfaction of enjoying each and every one.
+
+This script allows you to copy an existing playlist after randomizing
+the tracks. You can then play without shuffle and get the equivalent of
+what you actually wanted.
 """
 
 import base64
 import json
-import os
 import urllib
 
 from flask import Flask, request, redirect, g, render_template
@@ -34,21 +43,32 @@ from spotipy import util
 
 app = Flask(__name__)
 
-# Spotify URLS
-SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
-SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
-API_VERSION = "v1"
-SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
-
 # Server-side Parameters
 CLIENT_SIDE_URL = "http://127.0.0.1"
 PORT = 8080
 REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
 SCOPE = "playlist-modify-public playlist-modify-private"
-STATE = ""
-SHOW_DIALOG_bool = True
-SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+
+class AccessToken(object):
+    """Stores a Spotify access token."""
+    _access_token = None
+
+    @property
+    @classmethod
+    def access_token(cls):
+        return cls._access_token
+
+    @access_token.setter
+    @classmethod
+    def access_token(cls, access_token):
+        cls._access_token = access_token
+
+
+def get_oauth():
+    prefs = get_prefs()
+    return spotipy.oauth2.SpotifyOAuth(
+        prefs["ClientID"], prefs["ClientSecret"], REDIRECT_URI, scope=SCOPE)
 
 
 def get_prefs():
@@ -65,40 +85,17 @@ def get_prefs():
 
 @app.route("/")
 def index():
-    prefs = get_prefs()
-    sp_oauth = spotipy.oauth2.SpotifyOAuth(
-        prefs["ClientID"], prefs["ClientSecret"], REDIRECT_URI,
-        scope="user-library-read")
+    sp_oauth = get_oauth()
     return redirect(sp_oauth.get_authorize_url())
 
 
-@app.route("/callback/q")
-def callback():
-    prefs = get_prefs()
-    # Auth Step 4: Requests refresh and access tokens
-    auth_token = request.args['code']
-    code_payload = {
-        "grant_type": "authorization_code",
-        "code": str(auth_token),
-        "redirect_uri": REDIRECT_URI
-    }
-    base64encoded = base64.b64encode("{}:{}".format(prefs["ClientID"],
-                                                    prefs["ClientSecret"]))
-    headers = {"Authorization": "Basic {}".format(base64encoded)}
-    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload,
-                                 headers=headers)
-
-    # Auth Step 5: Tokens are Returned to Application
-    response_data = json.loads(post_request.text)
-    access_token = response_data["access_token"]
-    refresh_token = response_data["refresh_token"]
-    token_type = response_data["token_type"]
-    expires_in = response_data["expires_in"]
-
-    sp = spotipy.Spotify(access_token)
+@app.route("/playlist/<playlist_id>")
+def tracks(playlist_id):
+    # TODO: Probably need to use the refresh token here.
+    sp = spotipy.Spotify(AccessToken.access_token)
     user_id = sp.current_user()["id"]
     # TODO: Grab a playlist id...
-    results = sp.user_playlist(user_id, "5lKq6aIb91EFzmzmrfeVV3")
+    results = sp.user_playlist(user_id, playlist_id)
 
     tracks = results["tracks"]
     track_names = [track["track"]["name"] for track in tracks["items"]]
@@ -108,6 +105,31 @@ def callback():
                             tracks["items"]])
 
     return render_template("index.html", sorted_array=track_names)
+
+
+@app.route("/callback/q")
+def callback():
+    # Auth Step 4: Requests refresh and access tokens
+    auth_token = request.args['code']
+
+    # # Auth Step 5: Tokens are Returned to Application
+    sp_oauth = get_oauth()
+    response_data = sp_oauth.get_access_token(auth_token)
+
+    AccessToken.access_token = response_data["access_token"]
+    spotify = spotipy.Spotify(AccessToken.access_token)
+    user_id = spotify.current_user()["id"]
+    results = spotify.user_playlists(user_id)
+
+    playlists = results["items"]
+    playlist_names = [{"id": playlist["id"], "name": playlist["name"]} for
+                      playlist in playlists]
+    while results["next"]:
+        results = sp.next(playlists)
+        playlist_names.extend([{"id": playlist["id"], "name": playlist["name"]}
+                               for playlist in results])
+
+    return render_template("playlists.html", sorted_array=playlist_names)
 
 
 if __name__ == "__main__":
